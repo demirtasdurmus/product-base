45dc752c60bee52977e3a0ba8452dc5b5d22a53b
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2025-09-25 17:13:07 +0300

    chore(pmobile): fix android network problem

--- apps/pback/src/config/index.ts
+++ apps/pback/src/config/index.ts
@@ -3,7 +3,7 @@ import z from 'zod';
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
   PBACK_PORT: z.coerce.number().default(3000),
-  PBACK_HOST: z.string().default('localhost'),
+  PBACK_HOST: z.string().default('0.0.0.0'),
   BETTER_AUTH_SECRET: z.string({ error: 'BETTER_AUTH_SECRET is required' }).min(10),
   BETTER_AUTH_URL: z.url({ error: 'BETTER_AUTH_URL is required' }),
   DATABASE_URL: z
--- apps/pmobile/src/app/index.tsx
+++ apps/pmobile/src/app/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Ionicons from '@expo/vector-icons/AntDesign';
-import { router, useNavigationContainerRef } from 'expo-router';
-import { Alert, Image, View } from 'react-native';
+import { Redirect, router, useNavigationContainerRef } from 'expo-router';
+import { ActivityIndicator, Alert, Image, View } from 'react-native';
 import { Button } from '../components/button';
 import { Card, CardFooter, CardHeader, CardTitle } from '../components/card';
 import { Input } from '../components/input';
@@ -10,11 +10,24 @@ import { Text } from '../components/text';
 import { authClient } from '../lib/auth-client';
 
 export default function Index() {
-  const { data: isAuthenticated } = authClient.useSession();
+  const { data: isAuthenticated, isPending } = authClient.useSession();
   const navContainerRef = useNavigationContainerRef();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleSignIn = () => {
+    authClient.signIn.email(
+      {
+        email,
+        password
+      },
+      {
+        onError: (ctx) => {
+          Alert.alert(ctx.error.message);
+        }
+      }
+    );
+  };
   useEffect(() => {
     if (isAuthenticated) {
       if (navContainerRef.isReady()) {
@@ -23,6 +36,13 @@ export default function Index() {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuthenticated, navContainerRef.isReady()]);
+
+  if (isPending) {
+    return <ActivityIndicator />;
+  }
+  if (isAuthenticated) {
+    return <Redirect href="/dashboard" />;
+  }
   return (
     <Card className="z-50 mx-6 bg-gray-200/70 backdrop-blur-lg">
       <CardHeader className="flex items-center justify-center gap-8">
@@ -35,6 +55,7 @@ export default function Index() {
         />
         <CardTitle>Sign In to your account</CardTitle>
       </CardHeader>
+
       <View className="flex gap-2 px-6">
         <Button
           onPress={() => {
@@ -63,11 +84,13 @@ export default function Index() {
           <Text>Sign In with GitHub</Text>
         </Button>
       </View>
+
       <View className="my-4 w-full flex-row items-center gap-2 px-6">
         <Separator className="w-3/12 grow" />
         <Text>or continue with</Text>
         <Separator className="w-3/12 grow" />
       </View>
+
       <View className="px-6">
         <Input
           placeholder="Email Address"
@@ -87,6 +110,7 @@ export default function Index() {
           }}
         />
       </View>
+
       <CardFooter>
         <View className="w-full">
           <Button
@@ -98,23 +122,10 @@ export default function Index() {
           >
             <Text className="text-center underline">Forget Password?</Text>
           </Button>
-          <Button
-            onPress={() => {
-              authClient.signIn.email(
-                {
-                  email,
-                  password
-                },
-                {
-                  onError: (ctx) => {
-                    Alert.alert(ctx.error.message);
-                  }
-                }
-              );
-            }}
-          >
+          <Button onPress={handleSignIn}>
             <Text>Continue</Text>
           </Button>
+
           <Text className="mt-2 text-center">
             Don't have an account?{' '}
             <Text
