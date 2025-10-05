b64536d930c9d74a19347117599e5d3d601ca549
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2025-10-05 09:17:58 +0300

    chore: add timezone to db timestamp fields

--- libs/backend/src/models/sample.ts
+++ libs/backend/src/models/sample.ts
@@ -1,6 +1,11 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 
 export const sample = pgTable('sample', {
   id: integer().primaryKey().notNull(),
-  name: varchar({ length: 255 }).notNull()
+  name: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp('updated_at', { withTimezone: true })
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull()
 });
