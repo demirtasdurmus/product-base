edce88c5a8ef155dc517fe4507df1d5bfcb6d955
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2025-10-18 22:40:58 +0300

    chore: add pmobile-e2e with detox

--- tools/scripts/eas-build-post-install.mjs
+++ tools/scripts/eas-build-post-install.mjs
@@ -5,10 +5,16 @@
  * It will create a symlink from the project's node_modules to the workspace's node_modules.
  */
 
+import { execSync } from 'child_process';
 import { existsSync, symlink } from 'fs';
 import { join } from 'path';
 
 const [workspaceRoot, projectRoot] = process.argv.slice(2);
+
+/**
+ * Build libraries that the project depends on
+ */
+buildLibraries(['shared']);
 
 if (existsSync(join(workspaceRoot, 'node_modules'))) {
   console.log('Symlink already exists');
@@ -21,3 +27,21 @@ symlink(join(projectRoot, 'node_modules'), join(workspaceRoot, 'node_modules'), 
     console.log('Symlink created');
   }
 });
+
+/**
+ * Build any other libraries that the project depends on
+ * @param {string[]} libraryNames
+ * @returns {void}
+ */
+function buildLibraries(libraryNames) {
+  try {
+    execSync(`nx run-many -t=build -p=${libraryNames.join(',')}`, {
+      cwd: workspaceRoot,
+      stdio: 'inherit'
+    });
+    console.log(`${libraryNames.join(',')} built successfully`);
+  } catch (error) {
+    console.error(`Failed to build ${libraryNames.join(' ')}:`, error.message);
+    process.exit(1);
+  }
+}
