622dd7773e51254c33c01d63e2f96c69a3628314
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2025-12-23 10:22:38 +0300

    ci: add ncu config

---
+++ .ncurc.js
@@ -0,0 +1,13 @@
+/** @type {import('npm-check-updates').RcOptions } */
+module.exports = {
+  upgrade: true,
+  target: 'patch',
+  packageManager: 'npm',
+  packageFile: 'package.json',
+  reject: (name, _semver) => {
+    if (name.startsWith('@nx/') || name === 'nx') {
+      return true;
+    }
+    return false;
+  }
+};
